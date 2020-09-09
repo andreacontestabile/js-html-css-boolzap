94c146e48c8c929699d5e394d354201d19b62a0e
Andrea Contestabile <andreacontest@gmail.com>
2020-09-09 12:40:27 +0200

    Risposta automatica jQuery

--- js/script.js
+++ js/script.js
@@ -11,15 +11,53 @@ $(document).ready(function() {
   });
 });
 
+// FUNZIONI
 
+// Funzione Get Time
+function getTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  var currentTime = hours + ":" + minutes;
+
+  return currentTime;
+}
+
+// Funzione Send Message
 function sendMessage() {
 
   var inputValue = $("#msg-input").val();
   var msgElement = $(".msg-template .msg-row").clone();
   if (inputValue !="") {
+
+    var currentTime = getTime();
+
     msgElement.find(".msg-text").text(inputValue);
+    msgElement.find(".msg-time").text(currentTime);
+    msgElement.find(".msg").addClass("user-msg");
+
     $(".main-view").append(msgElement);
     $("#msg-input").val("");
+
+    getReply();
   };
 
+}
+
+// Funzione Get Reply
+function getReply() {
+  setTimeout(function(){
+    var msgElement = $(".msg-template .msg-row").clone();
+    var currentTime = getTime();
+
+    msgElement.find(".msg-text").text("Questa è una risposta");
+    msgElement.find(".msg-time").text(currentTime);
+    msgElement.find(".msg").addClass("ai-msg");
+
+    $(".main-view").append(msgElement);
+
+  }, 1000);
 }
